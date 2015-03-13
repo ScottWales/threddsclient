c1ea458e3e9aec16cfd2622cb7a57adde6250f8d
Scott Wales <scott.wales@unimelb.edu.au>
2015-03-13 17:23:03 +1100

    Add dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 from setuptools import setup
 
 setup(
-        name         = 'threddsclient',
-        version      = '0.1.0',
-        description  = 'Thredds catalog client',
-        author       = 'Scott Wales',
-        author_email = 'scott.wales@unimelb.edu.au',
-        license      = 'Apache 2.0',
-        packages     = ['threddsclient'],
-        zip_safe     = False,
+        name             = 'threddsclient',
+        version          = '0.1.0',
+        description      = 'Thredds catalog client',
+        author           = 'Scott Wales',
+        author_email     = 'scott.wales@unimelb.edu.au',
+        license          = 'Apache 2.0',
+        packages         = ['threddsclient'],
+        zip_safe         = False,
+        install_requires = [
+            'requests',
+            'beautifulsoup4',
+            ],
         )
