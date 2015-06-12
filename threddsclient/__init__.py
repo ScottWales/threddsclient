78d6e97f393acbab891c710833e17da4fe1de863
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-06-12 18:47:19 +0200

    update imports

--- threddsclient/__init__.py
+++ threddsclient/__init__.py
@@ -1,3 +1,5 @@
-from .client import *
+import requests
+
+from .client import download_urls, opendap_urls, read_url, read_xml
 from .nodes import Service, CatalogRef, CollectionDataset, DirectDataset
 from .catalog import Catalog
