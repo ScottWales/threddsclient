9c60ca28af7bd8574e0e7a7798df805b97a6fdb1
Carsten Ehbrecht <carsten@linacs.org>
2015-06-13 17:11:46 +0200

    cleaned up exports

--- threddsclient/__init__.py
+++ threddsclient/__init__.py
@@ -1,5 +1,3 @@
 import requests
-
 from .client import download_urls, opendap_urls, read_url, read_xml
-from .nodes import Service, CatalogRef, CollectionDataset, DirectDataset
-from .catalog import Catalog
+
