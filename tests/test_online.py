bdfbbce64dc0bd11f875c707c56b75445b238ee2
pingu <pingu@norah>
2015-06-14 00:00:19 +0200

    more online tests added

--- tests/test_online.py
+++ tests/test_online.py
@@ -3,6 +3,11 @@ import pytest
 from threddsclient import *
 
 
+def test_noaa():
+    cat = read_url('http://www.esrl.noaa.gov/psd/thredds/catalog.xml')
+    assert cat.name == 'THREDDS PSD Test Catalog'
+    assert cat.url == 'http://www.esrl.noaa.gov/psd/thredds/catalog.xml'
+
 def test_utas_tpac():
     cat = read_url('http://portal.sf.utas.edu.au/thredds/catalog.xml')
     assert cat.name == 'TPAC Digital Library Data Server Catalog'
