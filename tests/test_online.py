af60b6128b425a882bfaf9d96354ff604338fdbd
Carsten Ehbrecht <carsten@linacs.org>
2015-10-25 16:50:54 +0100

    disabled online tests for utas_tpac ... not available

--- tests/test_online.py
+++ tests/test_online.py
@@ -13,13 +13,13 @@ def test_noaa():
     assert cat2.url == 'http://www.esrl.noaa.gov/psd/thredds/catalog/Datasets/catalog.xml'
 
     
-def test_utas_tpac():
-    cat = read_url('http://portal.sf.utas.edu.au/thredds/catalog.xml')
-    assert cat.name == 'TPAC Digital Library Data Server Catalog'
-    assert cat.url == 'http://portal.sf.utas.edu.au/thredds/catalog.xml'
+## def test_utas_tpac():
+##     cat = read_url('http://portal.sf.utas.edu.au/thredds/catalog.xml')
+##     assert cat.name == 'TPAC Digital Library Data Server Catalog'
+##     assert cat.url == 'http://portal.sf.utas.edu.au/thredds/catalog.xml'
 
 
-def test_utas_tpac_html():
-    cat = read_url('http://portal.sf.utas.edu.au/thredds/catalog.html')
-    assert cat.name == 'TPAC Digital Library Data Server Catalog'
-    assert cat.url == 'http://portal.sf.utas.edu.au/thredds/catalog.xml'
+## def test_utas_tpac_html():
+##     cat = read_url('http://portal.sf.utas.edu.au/thredds/catalog.html')
+##     assert cat.name == 'TPAC Digital Library Data Server Catalog'
+##     assert cat.url == 'http://portal.sf.utas.edu.au/thredds/catalog.xml'
